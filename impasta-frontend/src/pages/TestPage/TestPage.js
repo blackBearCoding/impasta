51f1cf6ebe5bb2137d648509aabbe7b88b40731a
Jake Eisenberg <jacobeisenberg94@gmail.com>
2020-09-06 14:24:02 -0600

    IMPASTA-10 | Add Link import

--- impasta-frontend/src/pages/TestPage/TestPage.js
+++ impasta-frontend/src/pages/TestPage/TestPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import DrawingState from '../../pageStates/RoomPageStates/DrawingState/DrawingState';
 
 import './TestPage.css';
+import { Link } from 'react-router-dom';
 
 export default function TestPage(props) {
   let display;
